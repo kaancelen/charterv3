06784c48a33177a0d2f2e7700e68badaf88e1cd2
Kaan Celen <berat.kaan.celen@accenture.com>
2016-03-17 11:05:19 +0200

    empty charts

--- src/com/kaancelen/charterv3/controllers/PerformanceController.java
+++ src/com/kaancelen/charterv3/controllers/PerformanceController.java
@@ -13,6 +13,9 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
 
 import com.kaancelen.charterv3.models.ExcelFile;
 import com.kaancelen.charterv3.utils.Constants;
@@ -29,17 +32,31 @@ public class PerformanceController implements Serializable{
 	
 	private String[] monthList;
 	private List<String> selectedMonths;
+	
+	private BarChartModel personelChart;
+	private BarChartModel departmentChart;
+	private LineChartModel monthlyChart;
+	private BarChartModel compareChart;
 
 	@PostConstruct
 	public void init(){
 		System.out.println("PerformanceController#init");
 		excelFileList = new ArrayList<ExcelFile>();
 		monthList = Constants.MONTH_ARRAY;
+		resetCharts();
 	}
 	
 	@PreDestroy
 	public void destroy(){
 		System.out.println("PerformanceController#destroy");
+	}
+	
+	private void resetCharts(){
+		BarChartModel model = new BarChartModel();
+        model.addSeries(new ChartSeries("PREVENT NULL POINTER EXCEPTION"));
+        
+        personelChart = departmentChart = compareChart = model;//PREVENET NULL POINTER EXCEPTION
+        monthlyChart = new LineChartModel();//PREVENET NULL POINTER EXCEPTION
 	}
 	
 	public void handleFileUpload(FileUploadEvent event){
@@ -104,7 +121,37 @@ public class PerformanceController implements Serializable{
 	public void setMonthList(String[] monthList) {
 		this.monthList = monthList;
 	}
-	
-	
+
+	public BarChartModel getPersonelChart() {
+		return personelChart;
+	}
+
+	public void setPersonelChart(BarChartModel personelChart) {
+		this.personelChart = personelChart;
+	}
+
+	public BarChartModel getDepartmentChart() {
+		return departmentChart;
+	}
+
+	public void setDepartmentChart(BarChartModel departmentChart) {
+		this.departmentChart = departmentChart;
+	}
+
+	public LineChartModel getMonthlyChart() {
+		return monthlyChart;
+	}
+
+	public void setMonthlyChart(LineChartModel monthlyChart) {
+		this.monthlyChart = monthlyChart;
+	}
+
+	public BarChartModel getCompareChart() {
+		return compareChart;
+	}
+
+	public void setCompareChart(BarChartModel compareChart) {
+		this.compareChart = compareChart;
+	}
 	
 }
