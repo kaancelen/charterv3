078be71b461d376bba370577671f957049cdcdec
Kaan Çelen <bkaancelen@gmail.com>
2016-08-22 12:51:36 +0300

    environment fix

--- src/com/kaancelen/charterv3/filter/CharacterEncodingFilter.java
+++ src/com/kaancelen/charterv3/filter/CharacterEncodingFilter.java
@@ -19,8 +19,8 @@ public class CharacterEncodingFilter implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-	    response.setCharacterEncoding("UTF-8");
-	    request.setCharacterEncoding("UTF-8");
+//	    response.setCharacterEncoding("UTF-8");
+//	    request.setCharacterEncoding("UTF-8");
 	    chain.doFilter(request, response);
 
 	}
